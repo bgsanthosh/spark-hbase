6adaed32d1a029e7af3e0d4ee782c92cb93184c7
Santhosh B Gowda <santhosh@cloudera.com>
2021-01-08 17:35:25 +0530

    added phoenix read write

--- spark-hbase/src/main/scala/org/cloudera/e2e/spark/PhoenixReadWrite.scala
+++ spark-hbase/src/main/scala/org/cloudera/e2e/spark/PhoenixReadWrite.scala
@@ -21,13 +21,13 @@ object PhoenixReadWrite {
       val hbase_zookeeper_url = args(0)
       val dataSet = List((1L, "1", 1), (2L, "2", 2), (3L, "3", 3))
 
-      spark.sparkContext.parallelize(dataSet).saveToPhoenix("OUTPUT_TEST_TABLE1", Seq("ID","COL1","COL2"),zkUrl = Some(hbase_zookeeper_url))
+      spark.sparkContext.parallelize(dataSet).saveToPhoenix("OUTPUT_TEST_TABLE", Seq("ID","COL1","COL2"),zkUrl = Some(hbase_zookeeper_url))
 
 
       val df = spark.sqlContext
         .read
         .format("org.apache.phoenix.spark")
-        .options(Map("table" -> "OUTPUT_TEST_TABLE1", "zkUrl" -> hbase_zookeeper_url))
+        .options(Map("table" -> "OUTPUT_TEST_TABLE", "zkUrl" -> hbase_zookeeper_url))
         .load()
 
       df.show()
